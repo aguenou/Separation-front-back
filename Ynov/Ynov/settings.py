d37815af66bfce75911691831a8b1985e505ea85
aguenou <aguenouy96@gmail.com>
2023-02-03 14:10:53 +0100

    Installation de djangorestframework-simplejwt

--- Ynov/Ynov/settings.py
+++ Ynov/Ynov/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myApp1.apps.Myapp1Config',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,17 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 WSGI_APPLICATION = 'Ynov.wsgi.application'
 
--- Ynov/Ynov/urls.py
+++ Ynov/Ynov/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from myApp1.views import ArtisteAPIView, ArtisteViewset
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 router = routers.SimpleRouter()
 router.register('artiste', ArtisteViewset, basename='artiste')
@@ -25,4 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
